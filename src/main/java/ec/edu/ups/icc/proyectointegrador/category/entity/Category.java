792db26d347a58e0b5a902fc2399af0de22952a5
Sebastian Gomez <sebgomez395@gmail.com>
2026-07-23 21:15:29 -0500

    setup: configuración base del proyecto, Docker y conexión a base de datos

---
+++ src/main/java/ec/edu/ups/icc/proyectointegrador/category/entity/Category.java
@@ -0,0 +1,68 @@
+package ec.edu.ups.icc.proyectointegrador.category.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="categories")
+public class Category {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+    @Column(length = 255)
+    private String description;
+
+    @Column(nullable = false)
+    private Boolean active;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name, String description, Boolean active) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.active = active;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
