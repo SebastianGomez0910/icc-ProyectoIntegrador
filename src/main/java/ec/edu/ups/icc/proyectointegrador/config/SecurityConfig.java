0041bd38f3f246234a7239e2a70af510176322dc
isaac-315 <isaacalf.135@gmail.com>
2026-07-25 18:13:20 -0500

    Error corregido

--- src/main/java/ec/edu/ups/icc/proyectointegrador/config/SecurityConfig.java
+++ src/main/java/ec/edu/ups/icc/proyectointegrador/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/auth/**").permitAll()
                 .requestMatchers("/users/**", "/roles/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
             )
