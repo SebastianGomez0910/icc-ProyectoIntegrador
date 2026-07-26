6a95ed45924b98f549969ddffbb4dd80cad2a1a7
JorgePizarroL <jpizarrol1@est.ups.edu.ec>
2026-07-25 20:26:00 -0500

    Add ResourceNotFound BusinessRule

---
+++ src/main/java/ec/edu/ups/icc/proyectointegrador/common/exception/BusinessRuleException.java
@@ -0,0 +1,9 @@
+package ec.edu.ups.icc.proyectointegrador.common.exception;
+
+public class BusinessRuleException extends RuntimeException {
+    
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/ec/edu/ups/icc/proyectointegrador/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package ec.edu.ups.icc.proyectointegrador.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
+
