60c115a5a71a650f1954ee52844cd6c1286ba51e
JorgePizarroL <jpizarrol1@est.ups.edu.ec>
2026-07-27 09:54:05 -0500

    EventRegistrationController completo

--- src/main/java/ec/edu/ups/icc/proyectointegrador/registration/controller/EventRegistrationController.java
+++ src/main/java/ec/edu/ups/icc/proyectointegrador/registration/controller/EventRegistrationController.java
@@ -1,5 +1,22 @@
 package ec.edu.ups.icc.proyectointegrador.registration.controller;
 
+@RestController
+@RequestMapping("/events/{eventId}/registrations")
+
 public class EventRegistrationController {
+
+    private final RegistrationService registrationService;
+
+    public EventRegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('PARTICIPANT')")
+    public ResponseEntity<RegistrationResponseDto> register(
+            @PathVariable Long eventId, Authentication authentication) {
+        RegistrationResponseDto registration = registrationService.register(eventId, authentication.getName());
+        return new ResponseEntity<>(registration, HttpStatus.CREATED);
+    }
     
 }
