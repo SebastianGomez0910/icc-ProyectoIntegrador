5f8a26a8780803d2b20dd8b4f131b57819da00c3
JorgePizarroL <jpizarrol1@est.ups.edu.ec>
2026-07-27 20:55:25 -0500

    EventRegistrationController completo

--- src/main/java/ec/edu/ups/icc/proyectointegrador/registration/controller/EventRegistrationController.java
+++ src/main/java/ec/edu/ups/icc/proyectointegrador/registration/controller/EventRegistrationController.java
@@ -1,5 +1,17 @@
 package ec.edu.ups.icc.proyectointegrador.registration.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ec.edu.ups.icc.proyectointegrador.registration.dto.RegistrationResponseDto;
+import ec.edu.ups.icc.proyectointegrador.registration.service.RegistrationService;
+
 @RestController
 @RequestMapping("/events/{eventId}/registrations")
 
