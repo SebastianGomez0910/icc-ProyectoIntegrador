b2e283b3c1dcf288dfd6bd856ba756db6d328314
Sebastian Gomez <sebgomez395@gmail.com>
2026-07-29 12:19:56 -0500

    cmabiio en securityfilterchain

--- src/main/java/ec/edu/ups/icc/proyectointegrador/config/SecurityConfig.java
+++ src/main/java/ec/edu/ups/icc/proyectointegrador/config/SecurityConfig.java
@@ -27,7 +27,6 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
@@ -106,17 +105,16 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/actuator/health").permitAll()
                 .requestMatchers("/auth/login", "/auth/register", "/auth/refresh").permitAll()
                 .requestMatchers("/status/**").permitAll()
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                 .requestMatchers("/categories/**").authenticated()
-                .requestMatchers("/actuator/health").permitAll()
-
+                
                 .requestMatchers("/error").permitAll()
                 .anyRequest().authenticated()
             );
 
-        http.authenticationProvider(authenticationProvider());
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
